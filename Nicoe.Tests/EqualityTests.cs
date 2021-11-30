a9f3b8be57afcf0b1beccb74945f629ca1cf3cfd
libertyernie <maybeway36@gmail.com>
2021-11-30 15:22:58 -0600

    Add NintendontConfiguration wrapper class

---
+++ Nicoe.Tests/EqualityTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Nicoe.Tests {
+    [TestClass]
+    public class EqualityTests {
+        [TestMethod]
+        public void TestDefaultEquality() {
+            var o1 = new NintendontConfiguration();
+            var o2 = new NintendontConfiguration();
+            Assert.IsFalse(o1 == o2);
+            Assert.IsTrue(o1.Equals(o2));
+        }
+    }
+}
