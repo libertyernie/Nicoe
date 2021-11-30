550c6c529da7e0d9993f008e36123dcee038260c
libertyernie <maybeway36@gmail.com>
2021-11-30 15:23:04 -0600

    Copy config reading and updating methods from Nintendont

--- Nicoe/NintendontConfiguration.cpp
+++ Nicoe/NintendontConfiguration.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 
 #include "CommonConfig.h"
+#include "ByteOrderConversion.h"
 
 using namespace System;
 using namespace System::Runtime::InteropServices;
@@ -10,6 +11,94 @@ using namespace System::IO;
 public ref class NintendontConfiguration {
 private:
 	NIN_CFG* ncfg;
+
+	bool LoadNinCFG(String^ path)
+	{
+		bool ConfigLoaded = true;
+
+		int64_t BytesRead;
+
+		{
+			FileStream inStream(path, FileMode::Open, FileAccess::Read);
+			UnmanagedMemoryStream outStream((uint8_t*)ncfg, sizeof(NIN_CFG), sizeof(NIN_CFG), FileAccess::Write);
+			inStream.CopyTo(% outStream);
+			BytesRead = inStream.Position;
+		}
+
+		switch (ncfg->Version) {
+		case 2:
+			if (BytesRead != 540)
+				ConfigLoaded = false;
+			break;
+
+		default:
+			if (BytesRead != sizeof(NIN_CFG))
+				ConfigLoaded = false;
+			break;
+		}
+
+		if (ncfg->Magicbytes != 0x01070CF6)
+			ConfigLoaded = false;
+
+		UpdateNinCFG();
+
+		if (ncfg->Version != NIN_CFG_VERSION)
+			ConfigLoaded = false;
+
+		if (ncfg->MaxPads > NIN_CFG_MAXPAD)
+			ConfigLoaded = false;
+
+		return ConfigLoaded;
+	}
+
+	void UpdateNinCFG()
+	{
+		if (ncfg->Version == 2)
+		{	//251 blocks, used to be there
+			ncfg->NetworkProfile = 0x2;
+			ncfg->Version = 3;
+		}
+		if (ncfg->Version == 3)
+		{	//new memcard setting space
+			ncfg->MemCardBlocks = ncfg->NetworkProfile;
+			ncfg->VideoScale = 0;
+			ncfg->VideoOffset = 0;
+			ncfg->Version = 4;
+		}
+		if (ncfg->Version == 4)
+		{	//Option got changed so not confuse it
+			ncfg->Config &= ~NIN_CFG_HID;
+			ncfg->Version = 5;
+		}
+		if (ncfg->Version == 5)
+		{	//New Video Mode option
+			ncfg->VideoMode &= ~NIN_VID_PATCH_PAL50;
+			ncfg->Version = 6;
+		}
+		if (ncfg->Version == 6)
+		{	//New flag, disabled by default
+			ncfg->Config &= ~NIN_CFG_ARCADE_MODE;
+			ncfg->Version = 7;
+		}
+		if (ncfg->Version == 7)
+		{	// Wiimote CC Rumble, disabled by default;
+			// don't skip IPL by default.
+			ncfg->Config &= ~NIN_CFG_CC_RUMBLE;
+			ncfg->Config &= ~NIN_CFG_SKIP_IPL;
+			ncfg->Version = 8;
+		}
+		if (ncfg->Version == 8)
+		{	// BBA Emu disabled by default
+			// BBA Config 0 by default
+			ncfg->Config &= ~NIN_CFG_BBA_EMU;
+			ncfg->NetworkProfile = 0;
+			ncfg->Version = 9;
+		}
+		if (ncfg->Version == 9) {
+			ncfg->WiiUGamepadSlot = 0;
+			ncfg->Version = 10;
+		}
+	}
 public:
 	NintendontConfiguration() {
 		ncfg = (NIN_CFG*)malloc(sizeof(NIN_CFG));
@@ -25,7 +114,12 @@ public:
 
 	void Reset() {
 		memset(ncfg, 0, sizeof(NIN_CFG));
-		throw gcnew NotImplementedException();
+
+		ncfg->Magicbytes = 0x01070CF6;
+		ncfg->Version = NIN_CFG_VERSION;
+		ncfg->Language = NIN_LAN_AUTO;
+		ncfg->MaxPads = NIN_CFG_MAXPAD;
+		ncfg->MemCardBlocks = 0x2;//251 blocks
 	}
 
 	~NintendontConfiguration() {
