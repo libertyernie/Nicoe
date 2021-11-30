3342d05fada025fa6cdcbf551a5d779689562429
libertyernie <maybeway36@gmail.com>
2021-11-30 15:23:40 -0600

    Allocate open/save dialogs when needed

--- Nicoe.Gui/Form1.Designer.vb
+++ Nicoe.Gui/Form1.Designer.vb
@@ -22,7 +22,9 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.PropertyGrid1 = New System.Windows.Forms.PropertyGrid()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
@@ -34,27 +36,44 @@ Partial Class Form1
         Me.ExportBannerImageToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.PropertyGrid1 = New System.Windows.Forms.PropertyGrid()
-        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
-        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
-        Me.OpenFileDialogXml = New System.Windows.Forms.OpenFileDialog()
-        Me.SaveFileDialogXml = New System.Windows.Forms.SaveFileDialog()
-        Me.SaveFileDialogPng = New System.Windows.Forms.SaveFileDialog()
-        Me.OpenFileDialogIso = New System.Windows.Forms.OpenFileDialog()
-        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.MenuStrip1.SuspendLayout()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.TableLayoutPanel1.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'MenuStrip1
+        'PropertyGrid1
         '
-        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.ToolsToolStripMenuItem, Me.HelpToolStripMenuItem})
-        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
-        Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(384, 24)
-        Me.MenuStrip1.TabIndex = 0
-        Me.MenuStrip1.Text = "MenuStrip1"
+        Me.PropertyGrid1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PropertyGrid1.Location = New System.Drawing.Point(0, 24)
+        Me.PropertyGrid1.Name = "PropertyGrid1"
+        Me.PropertyGrid1.Size = New System.Drawing.Size(384, 511)
+        Me.PropertyGrid1.TabIndex = 1
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.Button2, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button1, 0, 0)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 535)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(384, 26)
+        Me.TableLayoutPanel1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button1.Location = New System.Drawing.Point(3, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(186, 20)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Browse for GamePath"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'FileToolStripMenuItem
         '
@@ -125,64 +144,24 @@ Partial Class Form1
         Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(107, 22)
         Me.AboutToolStripMenuItem.Text = "&About"
         '
-        'PropertyGrid1
+        'MenuStrip1
         '
-        Me.PropertyGrid1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.PropertyGrid1.Location = New System.Drawing.Point(0, 24)
-        Me.PropertyGrid1.Name = "PropertyGrid1"
-        Me.PropertyGrid1.Size = New System.Drawing.Size(384, 511)
-        Me.PropertyGrid1.TabIndex = 1
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.ToolsToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(384, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
         '
-        'OpenFileDialog1
+        'Button2
         '
-        Me.OpenFileDialog1.DefaultExt = "bin"
-        '
-        'SaveFileDialog1
-        '
-        Me.SaveFileDialog1.FileName = "bin"
-        '
-        'OpenFileDialogXml
-        '
-        Me.OpenFileDialogXml.DefaultExt = "xml"
-        Me.OpenFileDialogXml.Filter = "Homebrew Channel meta.xml files (*.xml)|*.xml"
-        '
-        'SaveFileDialogXml
-        '
-        Me.SaveFileDialogXml.DefaultExt = "xml"
-        Me.SaveFileDialogXml.Filter = "Homebrew Channel meta.xml files (*.xml)|*.xml"
-        '
-        'SaveFileDialogPng
-        '
-        Me.SaveFileDialogPng.DefaultExt = "png"
-        Me.SaveFileDialogPng.Filter = "Portable Network Graphics (*.png)|*.png"
-        '
-        'OpenFileDialogIso
-        '
-        Me.OpenFileDialogIso.Filter = "GameCube disc images (*.iso, *.gcm)|*.iso;*.gcm"
-        '
-        'TableLayoutPanel1
-        '
-        Me.TableLayoutPanel1.ColumnCount = 2
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.TableLayoutPanel1.Controls.Add(Me.Button1, 0, 0)
-        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 535)
-        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
-        Me.TableLayoutPanel1.RowCount = 1
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.TableLayoutPanel1.Size = New System.Drawing.Size(384, 26)
-        Me.TableLayoutPanel1.TabIndex = 2
-        '
-        'Button1
-        '
-        Me.Button1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Button1.Location = New System.Drawing.Point(3, 3)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(186, 20)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Browse for GamePath"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button2.Location = New System.Drawing.Point(195, 3)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(186, 20)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Browse for CheatPath"
+        Me.Button2.UseVisualStyleBackColor = True
         '
         'Form1
         '
@@ -195,33 +174,27 @@ Partial Class Form1
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
         Me.Text = "Nicoe"
+        Me.TableLayoutPanel1.ResumeLayout(False)
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
-        Me.TableLayoutPanel1.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents PropertyGrid1 As PropertyGrid
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Button1 As Button
     Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents SaveAsToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ImportMetaxmlForAutobootToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExportMetaxmlForAutobootToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExportBannerImageToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents PropertyGrid1 As PropertyGrid
-    Friend WithEvents OpenFileDialog1 As OpenFileDialog
-    Friend WithEvents SaveFileDialog1 As SaveFileDialog
-    Friend WithEvents ToolsToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents ExportBannerImageToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents ExportMetaxmlForAutobootToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents OpenFileDialogXml As OpenFileDialog
-    Friend WithEvents SaveFileDialogXml As SaveFileDialog
-    Friend WithEvents SaveFileDialogPng As SaveFileDialog
-    Friend WithEvents ImportMetaxmlForAutobootToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents OpenFileDialogIso As OpenFileDialog
-    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
-    Friend WithEvents Button1 As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents Button2 As Button
 End Class
--- Nicoe.Gui/Form1.vb
+++ Nicoe.Gui/Form1.vb
@@ -33,15 +33,21 @@ Public Class Form1
     End Sub
 
     Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
-        If OpenFileDialog1.ShowDialog(Me) = DialogResult.OK Then
-            LoadFile(OpenFileDialog1.FileName)
-        End If
+        Using dialog As New OpenFileDialog
+            dialog.Filter = "Nintendont configuration files (*.bin)|*.bin"
+            If dialog.ShowDialog(Me) = DialogResult.OK Then
+                LoadFile(dialog.FileName)
+            End If
+        End Using
     End Sub
 
     Private Sub SaveAsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveAsToolStripMenuItem.Click
-        If SaveFileDialog1.ShowDialog(Me) = DialogResult.OK Then
-            File.WriteAllBytes(SaveFileDialog1.FileName, ConfigurationWrapper.Export())
-        End If
+        Using dialog As New SaveFileDialog
+            dialog.Filter = "Nintendont configuration files (*.bin)|*.bin"
+            If dialog.ShowDialog(Me) = DialogResult.OK Then
+                File.WriteAllBytes(dialog.FileName, ConfigurationWrapper.Export())
+            End If
+        End Using
     End Sub
 
     Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
@@ -59,54 +65,60 @@ https://github.com/FIX94/Nintendont")
     End Sub
 
     Private Sub ImportMetaxmlForAutobootToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ImportMetaxmlForAutobootToolStripMenuItem.Click
-        If OpenFileDialogXml.ShowDialog(Me) = DialogResult.OK Then
-            Using fs As New FileStream(OpenFileDialogXml.FileName, FileMode.Open, FileAccess.Read)
-                Dim xml = CType(New XmlSerializer(GetType(MetaXml)).Deserialize(fs), MetaXml)
-                Dim base64 = xml?.Arguments?.FirstOrDefault()
-                If base64 Is Nothing Then
-                    MsgBox("Could not find a base-64-encoded nincfg.dat in the first argument in meta.xml.")
-                Else
-                    Dim data = Convert.FromBase64String(base64)
-                    ConfigurationWrapper.Load(data)
-                    LastDataLoaded = ConfigurationWrapper.Export()
-                    PropertyGrid1.Refresh()
-                End If
-            End Using
-        End If
+        Using dialog As New OpenFileDialog
+            dialog.Filter = "Homebrew Channel meta.xml (*.xml)|*.xml"
+            If dialog.ShowDialog(Me) = DialogResult.OK Then
+                Using fs As New FileStream(dialog.FileName, FileMode.Open, FileAccess.Read)
+                    Dim xml = CType(New XmlSerializer(GetType(MetaXml)).Deserialize(fs), MetaXml)
+                    Dim base64 = xml?.Arguments?.FirstOrDefault()
+                    If base64 Is Nothing Then
+                        MsgBox("Could not find a base-64-encoded nincfg.dat in the first argument in meta.xml.")
+                    Else
+                        Dim data = Convert.FromBase64String(base64)
+                        ConfigurationWrapper.Load(data)
+                        LastDataLoaded = ConfigurationWrapper.Export()
+                        PropertyGrid1.Refresh()
+                    End If
+                End Using
+            End If
+        End Using
     End Sub
 
     Private Async Sub ExportMetaxmlForAutobootToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExportMetaxmlForAutobootToolStripMenuItem.Click
         Try
             MsgBox("This feature requires a modified build of Nintendont that can read the base64-encoded contents of a nincfg.bin file from a command-line argument.")
             Dim bnr = Await Banner.ExportGameCubeBanner(ConfigurationWrapper.GamePath)
-            If SaveFileDialogXml.ShowDialog(Me) = DialogResult.OK Then
-                If Not ConfigurationWrapper.AUTO_BOOT Then
-                    Dim result = MsgBox("Auto boot is off. Would you like to turn it on?", MsgBoxStyle.YesNoCancel)
-                    If result = MsgBoxResult.Cancel Then
-                        Return
-                    ElseIf result = MsgBoxResult.yes Then
-                        ConfigurationWrapper.AUTO_BOOT = True
-                        PropertyGrid1.Refresh()
+            Using dialog As New OpenFileDialog
+                dialog.Filter = "Homebrew Channel meta.xml (*.xml)|*.xml"
+                If dialog.ShowDialog(Me) = DialogResult.OK Then
+                    If Not ConfigurationWrapper.AUTO_BOOT Then
+                        Dim result = MsgBox("Auto boot is off. Would you like to turn it on?", MsgBoxStyle.YesNoCancel)
+                        If result = MsgBoxResult.Cancel Then
+                            Return
+                        ElseIf result = MsgBoxResult.Yes Then
+                            ConfigurationWrapper.AUTO_BOOT = True
+                            PropertyGrid1.Refresh()
+                        End If
                     End If
+
+                    Dim meta As New MetaXml With {
+                        .Version = 1,
+                        .Name = bnr.GameLong,
+                        .Coder = bnr.DeveloperLong,
+                        .ShortDescription = "",
+                        .LongDescription = bnr.DescriptionString,
+                        .NoIosReload = True,
+                        .AhbAccess = True,
+                        .Arguments = New String() {Convert.ToBase64String(ConfigurationWrapper.Export())}
+                    }
+
+                    Using ms As New MemoryStream()
+                        Dim serializer As New XmlSerializer(GetType(MetaXml))
+                        serializer.Serialize(ms, meta)
+                        File.WriteAllBytes(dialog.FileName, ms.ToArray())
+                    End Using
                 End If
-
-                Dim meta As New MetaXml With {
-                    .Version = 1,
-                    .Name = bnr.GameLong,
-                    .Coder = bnr.DeveloperLong,
-                    .ShortDescription = "",
-                    .LongDescription = bnr.DescriptionString,
-                    .NoIosReload = True,
-                    .AhbAccess = True,
-                    .Arguments = New String() {Convert.ToBase64String(ConfigurationWrapper.Export())}
-                }
-
-                Using ms As New MemoryStream()
-                    Dim serializer As New XmlSerializer(GetType(MetaXml))
-                    serializer.Serialize(ms, meta)
-                    File.WriteAllBytes(SaveFileDialogXml.FileName, ms.ToArray())
-                End Using
-            End If
+            End Using
         Catch ex As Exception
             MsgBox($"Could not export GameCube banner data due to an unknown error. ({ex.GetType().Name}: {ex.Message})")
         End Try
@@ -116,25 +128,45 @@ https://github.com/FIX94/Nintendont")
         Try
             Dim bnr = Await Banner.ExportGameCubeBanner(ConfigurationWrapper.GamePath)
             Dim image = bnr.GetImage()
-            If SaveFileDialogPng.ShowDialog() = DialogResult.OK Then
-                image.Save(SaveFileDialogPng.FileName)
-            End If
+            Using dialog As New SaveFileDialog
+                dialog.Filter = "PNG images (*.png)|*.png"
+                If dialog.ShowDialog() = DialogResult.OK Then
+                    image.Save(dialog.FileName)
+                End If
+            End Using
         Catch ex As Exception
             MsgBox($"Could not export GameCube banner data due to an unknown error. ({ex.GetType().Name}: {ex.Message})")
         End Try
     End Sub
 
     Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        If OpenFileDialogIso.ShowDialog() = DialogResult.OK Then
-            Dim path = OpenFileDialogIso.FileName.Replace("\", "/")
-            While path.Length > 0 And path(0) <> "/"c
-                path = path.Substring(1)
-            End While
+        Using dialog As New OpenFileDialog
+            dialog.Filter = "GameCube disc images (*.iso, *.gcm)|*.iso;*.gcm"
+            If dialog.ShowDialog() = DialogResult.OK Then
+                Dim path = dialog.FileName.Replace("\", "/")
+                While path.Length > 0 And path(0) <> "/"c
+                    path = path.Substring(1)
+                End While
 
-            ConfigurationWrapper.GamePath = path
-            ConfigurationWrapper.GameID = Await Banner.ReadGameId(path)
-            PropertyGrid1.Refresh()
-        End If
+                ConfigurationWrapper.GamePath = path
+                ConfigurationWrapper.GameID = Await Banner.ReadGameId(path)
+                PropertyGrid1.Refresh()
+            End If
+        End Using
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Using dialog As New OpenFileDialog
+            If dialog.ShowDialog() = DialogResult.OK Then
+                Dim path = dialog.FileName.Replace("\", "/")
+                While path.Length > 0 And path(0) <> "/"c
+                    path = path.Substring(1)
+                End While
+
+                ConfigurationWrapper.CheatPath = path
+                PropertyGrid1.Refresh()
+            End If
+        End Using
     End Sub
 
     Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
@@ -143,11 +175,14 @@ https://github.com/FIX94/Nintendont")
             If result = MsgBoxResult.Cancel Then
                 e.Cancel = True
             ElseIf result = MsgBoxResult.Yes Then
-                If SaveFileDialog1.ShowDialog(Me) = DialogResult.OK Then
-                    File.WriteAllBytes(SaveFileDialog1.FileName, ConfigurationWrapper.Export())
-                Else
-                    e.Cancel = True
-                End If
+                Using dialog As New SaveFileDialog
+                    dialog.Filter = "Nintendont configuration files (*.bin)|*.bin"
+                    If dialog.ShowDialog(Me) = DialogResult.OK Then
+                        File.WriteAllBytes(dialog.FileName, ConfigurationWrapper.Export())
+                    Else
+                        e.Cancel = True
+                    End If
+                End Using
             End If
         End If
     End Sub
